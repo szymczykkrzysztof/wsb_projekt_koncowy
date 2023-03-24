96f8790f8cef2a867340b26b8557dad6f75996e6
Krzysztof Szymczyk <Krzysztof.Szymczyk@fenergo.com>
2023-03-24 15:12:19 +0100

    added selectors

--- Pages/login_page.py
+++ Pages/login_page.py
@@ -1,6 +1,12 @@
+from selenium.webdriver.common.by import By
+
 from Pages.page_base import PageBase
 
 
 class LoginPage(PageBase):
     def login(self):
         self.check_title('Logowanie - EMPIK')
+        self.enter_txt((By.ID, "user-email"), "krzysztof.szymczyk@yahoo.com")
+        self.click_on((By.XPATH, "//button[text()='DALEJ']"), 60)
+        self.enter_txt((By.ID, "user-password"), "")
+        self.click_on((By.XPATH, "//button[text()='ZALOGUJ SIĘ']"), 60)
